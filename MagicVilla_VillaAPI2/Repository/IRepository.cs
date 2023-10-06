7b52b5d2315d63fbb174da608824f382264a037a
Jenny Duong <jduong20@CougarNet.UH.EDU>
2023-10-06 17:09:43 -0500

    completed and accepted

---
+++ MagicVilla_VillaAPI2/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using MagicVilla_VillaAPI2.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI2.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+
+        Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true);
+
+        Task CreateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task SaveAsync();
+    }
+}
--- MagicVilla_VillaAPI2/Repository/IVillaRepository.cs
+++ MagicVilla_VillaAPI2/Repository/IVillaRepository.cs
@@ -3,19 +3,12 @@ using System.Linq.Expressions;
 
 namespace MagicVilla_VillaAPI2.Repository
 {
-    public interface IVillaRepository
+    public interface IVillaRepository : IRepository<Villa>
     {
-        Task<List<Villa>> GetAllAsync(Expression<Func<Villa, bool>> filter = null);
 
-        Task<Villa> GetAsync(Expression<Func<Villa, bool>> filter = null, bool tracked = true);
-
-        Task CreateAsync(Villa entity);
-        Task RemoveAsync(Villa entity);
-
-        Task UpdateAsync(Villa entity);
+        Task <Villa> UpdateAsync(Villa entity);
 
 
-        Task SaveAsync();
 
     }
 }
